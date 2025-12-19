4adbba07c344e4dd20c036ad4f5bd6d4d8b2abfe
Mine <air1345@gmail.com>
2025-12-19 14:47:03 +0500

    Create fireC.js

---
+++ fireC.js
@@ -0,0 +1,13 @@
+var firebaseConfigCard = {
+  apiKey: "AIzaSyB2JTBAGNR64d_yRN4g8q9d1GpMfiiHxkE",
+  authDomain: "kcardentry.firebaseapp.com",
+  projectId: "kcardentry",
+  storageBucket: "kcardentry.firebasestorage.app",
+  messagingSenderId: "897372470233",
+  appId: "1:897372470233:web:e672fc3683b4985b7b37c8",
+  measurementId: "G-B2D7VLDT9X"
+};
+
+firebase.initializeApp(firebaseConfigCard);
+
+var db = firebase.firestore();
