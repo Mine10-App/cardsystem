7e999a6613d6e4d82b48e688228ded4a23b1e45e
Mine <air1345@gmail.com>
2025-12-20 13:08:13 +0500

    Update Co.js

--- Co.js
+++ Co.js
@@ -4,3 +4,6 @@ const companyInfo = {
   address: "Velana International Airport",
   phone: "+960 304 6677"
 };
+// Make it available globally
+window.companyInfo = companyInfo;
+console.log("Co.js loaded:", companyInfo);
