eb03a5e3d3abff7f71c70f18742dc6669b030603
Mine <air1345@gmail.com>
2025-12-21 15:31:50 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyC04iaSqeA76oOk_KlrfbSY6NTTNgy5LOY",
+  authDomain: "qrcards-2378e.firebaseapp.com",
+  projectId: "qrcards-2378e",
+  storageBucket: "qrcards-2378e.firebasestorage.app",
+  messagingSenderId: "863969491550",
+  appId: "1:863969491550:web:b764e63a2847f2b80cd2cf",
+  measurementId: "G-ZJPY6EZM41"
+};
