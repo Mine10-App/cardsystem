9c06084db33d23ed690b52c6218e0c978e7939da
Mine <air1345@gmail.com>
2025-12-24 14:12:02 +0500

    Update fireR1.js

--- fireR1.js
+++ fireR1.js
@@ -1,4 +1,5 @@
-apiKey: "AIzaSyB2JTBAGNR64d_yRN4g8q9d1GpMfiiHxkE",
+const firebaseConfig = {
+  apiKey: "AIzaSyB2JTBAGNR64d_yRN4g8q9d1GpMfiiHxkE",
   authDomain: "kcardentry.firebaseapp.com",
   projectId: "kcardentry",
   storageBucket: "kcardentry.firebasestorage.app",
@@ -6,3 +7,9 @@ apiKey: "AIzaSyB2JTBAGNR64d_yRN4g8q9d1GpMfiiHxkE",
   appId: "1:897372470233:web:e672fc3683b4985b7b37c8",
   measurementId: "G-B2D7VLDT9X"
 };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+window.db = db; // important!
+
+
+
