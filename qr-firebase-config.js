fadfdb0cee54d5e06f5a11ba0d40d6a4c8ca1684
Mine <air1345@gmail.com>
2025-12-27 09:52:22 +0500

    Update qr-firebase-config.js

--- qr-firebase-config.js
+++ qr-firebase-config.js
@@ -5,13 +5,13 @@ let qrFirebaseConfig = null;
 function loadQRFirebaseConfig() {
   try {
     qrFirebaseConfig = {
-      apiKey: "AIzaSyC04iaSqeA76oOk_KlrfbSY6NTTNgy5LOY",
-      authDomain: "qrcards-2378e.firebaseapp.com",
-      projectId: "qrcards-2378e",
-      storageBucket: "qrcards-2378e.firebasestorage.app",
-      messagingSenderId: "863969491550",
-      appId: "1:863969491550:web:b764e63a2847f2b80cd2cf",
-      measurementId: "G-ZJPY6EZM41"
+      apiKey: "AIzaSyC9FuNjWn8Up4JyjYbABEF2aNjO2bSXdfM",
+      authDomain: "qr-scanner-4458e.firebaseapp.com",
+      projectId: "qr-scanner-4458e",
+      storageBucket: "qr-scanner-4458e.firebasestorage.app",
+      messagingSenderId: "1099262112748",
+      appId: "1:1099262112748:web:a18f206bcf893c894546c9",
+      measurementId: "G-15WXNEPKS9"
     };
     
     console.log("✅ QR Firebase config loaded");
